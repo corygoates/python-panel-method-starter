e037588cd77a64bc7c8b5be105137cc739f060f5
Cory <cory.goates@aggiemail.usu.edu>
2022-08-02 13:51:19 -0600

    Framework is working.

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import sys
 import json
 
 import numpy as np
-import pyvista as pv
 
-from panels import Panel
-from vkt import load_vtk
+from vtk import load_vtk, export_vtk
 
 if __name__=="__main__":
 
@@ -16,7 +14,8 @@ if __name__=="__main__":
 
     # Load geometry
     geometry_file = input_vals["geometry"]["file"]
-    N, panels, vertices = load_vtk(geometry_file)
+    N_panels, N_verts, panels, vertices = load_vtk(geometry_file)
+    print("Read in {0} panels and {1} vertices from {2}.".format(N_panels, N_verts, geometry_file))
 
 
 
@@ -31,4 +30,10 @@ if __name__=="__main__":
 
     # Write output
     output_file = input_vals["output"]["file"]
-    export_vtk(output_file, panels, vertices)
+    example_result = {
+        "name" : "example_result",
+        "type" : "scalar",
+        "value" : np.linspace(1, 2, N_panels)
+    }
+    export_vtk(output_file, panels, vertices, results=[example_result])
+    print("Wrote results to {0}.".format(output_file))
